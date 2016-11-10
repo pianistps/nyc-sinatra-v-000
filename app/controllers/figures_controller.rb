9326409ae02aef9602243d9b35a5286a34791b12
Perla Yates <pianistps@yahoo.com>
2016-11-10 05:28:56 +0000

    corrected form and created the instances oh

--- app/controllers/figures_controller.rb
+++ app/controllers/figures_controller.rb
@@ -6,7 +6,11 @@ class FiguresController < ApplicationController
   end
 
   post '/figures' do
+    @figure = Figure.create(params[:figure])
+    @title = Title.create(params[:title])
+    @figure.titles << @title
+    @landmark = Landmark.create(params[:landmark])
+    @figure.landmarks << @landmark
 
-    
   end
 end
