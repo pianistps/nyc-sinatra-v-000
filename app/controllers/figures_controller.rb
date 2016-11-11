2386f42fac6f31379733d8d45c0a8ce00ac1033a
Perla Yates <pianistps@yahoo.com>
2016-11-11 19:56:09 +0000

    finished the figures controller and forms

--- app/controllers/figures_controller.rb
+++ app/controllers/figures_controller.rb
@@ -11,6 +11,31 @@ class FiguresController < ApplicationController
     @figure.titles << @title
     @landmark = Landmark.create(params[:landmark])
     @figure.landmarks << @landmark
+  end
 
+  get '/figures' do
+    @figures = Figure.all
+
+    erb :'/figures/index'
+  end
+
+  get '/figures/:id' do
+    @figure = Figure.find_by_id(params[:id])
+    erb :'/figures/show'
+  end
+
+  get '/figures/:id/edit' do
+    @figure = Figure.find_by_id(params[:id])
+
+    erb :'/figures/edit'
+  end
+
+  post '/figures/:id' do
+    @figure = Figure.find_by_id(params[:id])
+    @figure.update(params[:figure])
+    if !params["landmark"][:name].empty?
+      @figure.landmarks << Landmark.create(params[:landmark])
+    end
+    redirect "/figures/#{figure.id}"
   end
 end
